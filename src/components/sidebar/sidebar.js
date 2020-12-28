3e050a5e1c48725a94f8df2e3e4713b5a3979ca3
rubenmrios <rubenmrios@outlook.es>
2020-12-28 12:35:01 +0100

    end commit change button

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import { ThemeProvider } from 'styled-components';
-import { GlobalStyles } from  '../../assets/js/global';
+import { GlobalStyles } from '../../assets/js/global';
 import { lightTheme, darkTheme } from '../../assets/js/theme';
 
 
-function Sidebar(){
+function Sidebar() {
   const [theme, setTheme] = useState('light');
   const toggleTheme = () => {
     if (theme === 'light') {
@@ -13,47 +13,48 @@ function Sidebar(){
       setTheme('light');
     }
   }
-return(
-    
+  return (
+
     <div className="sidebar" data-color="white" data-active-color="danger">
       <div className="logo">
-     
+
         <a href="#" className="simple-text logo-normal">
           Creative Dashboard
          <div className="logo-image-big">
-          </div> 
+          </div>
         </a>
       </div>
       <div className="sidebar-wrapper">
         <ul className="nav">
           <li className="active ">
             <a href="./dashboard.html">
-            <i className="fa fa-home" aria-hidden="true"></i>
+              <i className="fa fa-home" aria-hidden="true"></i>
 
               <p>Dashboard</p>
             </a>
           </li>
-        
+
           <li>
             <a href="https://github.com/rubenmrios/react-dark-mode">
-            <i className="fa fa-github" aria-hidden="true"></i>
+              <i className="fa fa-github" aria-hidden="true"></i>
               <p>GitHub</p>
             </a>
           </li>
-          <li>
-          <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
-      <>
-        <GlobalStyles />
-        <button className="btn btn-primary" onClick={toggleTheme}>Toggle theme</button>
-        <footer>
-        </footer>
-      </>
-    </ThemeProvider>
+          <li className="d-flex justify-content-center">
+
+            <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+              <>
+                <GlobalStyles />
+   
+            <button className="btn btn-dark" onClick={toggleTheme}>Dark Mode</button>
+              
+              </>
+            </ThemeProvider>
           </li>
         </ul>
       </div>
     </div>
-);
+  );
 
 }
 
